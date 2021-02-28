3c39843d33f6175b85e5030c746501a2c2552a69
dronperminov <dronperminov@gmail.com>
2021-02-28 14:39:00 +0300

    add tkinter example app

---
+++ tkinter_example/tkinter_main.py
@@ -0,0 +1,11 @@
+from tkinter_application import Application
+
+
+def main():
+    app = Application()
+    app.master.title('Sample application')
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
